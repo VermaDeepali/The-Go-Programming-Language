9c9d8c1bdf87ace1a8f6213d19c789f8d72bfd7b
Deepali Verma <41052882+VermaDeepali@users.noreply.github.com>
2026-01-06 21:04:26 +0530

    updated the booking app

--- booking-app/main.go
+++ booking-app/main.go
@@ -7,34 +7,43 @@ func main() {
 	conferenceName := "Go Conference" // only applies to var and not const
 	const conferenceTicket int = 50
 	var remainingTickets uint = 50 // unsigned int and it will not have negative number
+	// var bookings = [50]string{"Deep", "Sumi"}
+	// var bookings [50]string
+	var bookings []string // use slice
 
-	fmt.Printf("conferenceTicket is %T, remainingTickets is %T, conferenceName is %T\n", conferenceTicket, remainingTickets, conferenceName)
+	// fmt.Printf("conferenceTicket is %T, remainingTickets is %T, conferenceName is %T\n", conferenceTicket, remainingTickets, conferenceName)
 
 	fmt.Printf("Welcome to %v booking application!\n", conferenceName)
 	fmt.Printf("We have total of %v tickets and %v are still available.\n", conferenceTicket, remainingTickets)
 	fmt.Println("Get your ticket here to attend!")
 
-	var firstName string
-	var lastName string
-	var email string
-	var userTickets uint
-	// ask user for their name
-	fmt.Println("Enter your first name: ")
-	fmt.Scan(&firstName) // used for input , & is a pointer is a var that points to the memory address of another var
+	for {
 
-	fmt.Println("Enter your last name: ")
-	fmt.Scan(&lastName)
+		var firstName string
+		var lastName string
+		var email string
+		var userTickets uint
+		// ask user for their name
+		fmt.Println("Enter your first name: ")
+		fmt.Scan(&firstName) // used for input , & is a pointer is a var that points to the memory address of another var
 
-	fmt.Println("Enter your email: ")
-	fmt.Scan(&email)
+		fmt.Println("Enter your last name: ")
+		fmt.Scan(&lastName)
 
-	fmt.Println("Enter number of tickets: ")
-	fmt.Scan(&userTickets)
-	// firstName = "Deep"
-	// userTickets = 2
+		fmt.Println("Enter your email: ")
+		fmt.Scan(&email)
 
-	remainingTickets = remainingTickets - userTickets
+		fmt.Println("Enter number of tickets: ")
+		fmt.Scan(&userTickets)
+		// firstName = "Deep"
+		// userTickets = 2
 
-	fmt.Printf("Thank you %v %v for booking %v tickets. You'll receive a confirmation email at %v\n", firstName, lastName, userTickets, email)
-	fmt.Printf("%v tickets remaining for %v\n", remainingTickets, conferenceTicket)
+		remainingTickets = remainingTickets - userTickets
+		bookings = append(bookings, firstName+" "+lastName)
+
+		fmt.Printf("Thank you %v %v for booking %v tickets. You'll receive a confirmation email at %v\n", firstName, lastName, userTickets, email)
+		fmt.Printf("%v tickets remaining for %v\n", remainingTickets, conferenceTicket)
+
+		fmt.Printf("These are all our bookings: %v\n", bookings)
+	}
 }
